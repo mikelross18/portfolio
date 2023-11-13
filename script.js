3592d946fa89206e4d9616ceb04f39be92d1ad32
mikelross18 <80691814+mikelross18@users.noreply.github.com>
2023-11-13 09:51:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+
+            event.currentTarget.classList.add("active-link")
+            document.getElementById(tabname).classList.add("active-tab");
+        }
+         
+
+var sidemenus = document.getElementById("sidemenu");
+
+        function openmenu(){
+            sidemenu.style.right = "0";
+        }       
+    
+        function closemenu(){
+            sidemenu.style.right = "-200px";
+        }
+ 
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzQ-_j3RL02mTCzvjLX4yYiHbbMxM-2-kxIVsWw-JhTpjLyxGqKwgpnEIBxPOLERXKgyg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Your Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })   
+        
+
+        
